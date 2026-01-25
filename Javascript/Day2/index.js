3549bbe96a0933ee3018d907c94f4b96d3b5a362
mprashant1100 <prashantmishra1100@gmail.com>
2026-01-25 11:39:12 +0530

    feat: async sync code practice

--- Javascript/Day2/index.js
+++ Javascript/Day2/index.js
@@ -1,1 +1,21 @@
-console.log(x)
+
+const fs = require("fs");
+
+// async
+fs.readFile("file.txt", () => {      // RUNS LATER
+  console.log("File read");
+});
+
+console.log("Done");
+
+// Output //
+// Done
+// File Read
+
+// sync
+const data = fs.readFileSync("file.txt"); // blocks
+console.log("Done");
+
+// output is like //
+// File Read
+// Done
