b047518d62383e40e295e57729f976353e3e3500
mprashant1100 <prashantmishra1100@gmail.com>
2026-01-30 18:55:09 +0530

    learn: Callbacks, callback hell, and issue with error handling in callback

---
+++ Javascript/Day4_Callback_and_Promises/index.js
@@ -0,0 +1,37 @@
+///// Callbacks  /////
+
+// Example - 
+setTimeout(() => {
+  console.log("Done");         // give a function to setTimeout,
+}, 1000);                      // You’re saying: “Call this function later.”
+
+
+
+
+// Callback hell 
+getUser(id, (user) => {
+  getOrders(user, (orders) => {
+    getPayment(orders, (payment) => {
+      ship(payment, () => {
+        console.log("done");
+      });
+    });
+  });
+});
+
+
+
+
+
+// Error handling
+doSomething((err, result) => {
+  if (err) {
+    // handle
+  } else {
+    doAnother((err2) => {
+      if (err2) {
+        // handle again
+      }
+    });
+  }
+});
