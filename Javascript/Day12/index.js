902d5abfa70eba1fcbf1c3fe3deec64f93324a43
mprashant1100 <prashantmishra1100@gmail.com>
2026-03-12 23:47:05 +0530

    dev: Interview Trap question

---
+++ Javascript/Day12/index.js
@@ -0,0 +1,16 @@
+
+
+// Javascript Interview Trap Question
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1000);
+}
+
+
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1000);
+}
+
+
+
