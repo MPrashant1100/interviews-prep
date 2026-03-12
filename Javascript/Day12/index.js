7534cf3d236a5e70c906234def629915c293fedd
mprashant1100 <prashantmishra1100@gmail.com>
2026-03-12 23:47:40 +0530

    .

--- Javascript/Day12/index.js
+++ Javascript/Day12/index.js
@@ -5,12 +5,11 @@
 for (var i = 0; i < 3; i++) {
   setTimeout(() => console.log(i), 1000);
 }
-
+// Output - 333
 
 
 for (let i = 0; i < 3; i++) {
   setTimeout(() => console.log(i), 1000);
 }
-
-
+// Output - 012
 
