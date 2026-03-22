11daf304aa36e8b3742a15784c66a18c482170bd
mprashant1100 <prashantmishra1100@gmail.com>
2026-03-22 12:52:38 +0530

    pract: Practice a interview trap question which mostly asked in interviews

---
+++ React/Day2/index.js
@@ -0,0 +1,20 @@
+
+
+const [count, setCount] = useState(0);
+
+<button 
+onClick={() => {
+  setCount(count + 1)
+  setCount(count + 1)
+  }
+}
+>
+  Click
+</button>
+
+
+setCount(0 + 1);
+setCount(0 + 1);
+
+
+
