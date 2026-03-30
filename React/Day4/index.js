a323835115b0301a5a1b3c7b16cd37302ebc57be
mprashant1100 <prashantmishra1100@gmail.com>
2026-03-30 14:19:25 +0530

    learn: useMemo code practice

---
+++ React/Day4/index.js
@@ -0,0 +1,22 @@
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const result = expensiveCalculation(count);
+
+  return (
+    <>
+      <button onClick={() => 
+        setCount(count + 1)}
+        >
+         Click
+      </button>
+      <p>{result}</p>
+    </>
+  );
+}
+
+const result = useMemo(() => {
+  return expensiveCalculation(count);
+}, [count]);
